29db892353c76b44bef67945293323808f1b9a89
vineeth bhaskaran <vineethbhaskaran@vineeths-MacBook-Air.local>
2021-10-18 11:06:46 +0530

    added post 3

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -10,4 +10,10 @@ var store = [{
         "tags": [],
         "url": "/post2/",
         "teaser": null
+      },{
+        "title": "Welcome to Jekyll Post 3!",
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/post3/",
+        "teaser": null
       }]
