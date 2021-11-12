e7465085dd795a9e04ed784a50bdee7f4c65ff6b
vineeth bhaskaran <vineethbhaskaran@vineeths-MacBook-Air.local>
2021-11-12 10:59:18 +0530

    changed photo

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -4,16 +4,4 @@ var store = [{
         "tags": [],
         "url": "/post1/",
         "teaser": null
-      },{
-        "title": "Welcome to Jekyll Post 2!",
-        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
-        "tags": [],
-        "url": "/post2/",
-        "teaser": null
-      },{
-        "title": "Welcome to Jekyll Post 3!",
-        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
-        "tags": [],
-        "url": "/post3/",
-        "teaser": null
       }]
